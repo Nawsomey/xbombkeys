2121d3c48f18019288f4197871fb6b5aad1dca47
Nawsomey <111446394+Nawsomey@users.noreply.github.com>
2024-12-08 10:34:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 const app = express();
 
 // Setup your KeyAuth API credentials here
-const KEYAUTH_APP_NAME = 'YourAppName';
-const KEYAUTH_OWNER_ID = 'YourOwnerID';
+const KEYAUTH_APP_NAME = 'Nawsifsspammail's Application';
+const KEYAUTH_OWNER_ID = 'vNCzuzTjmw';
 const KEYAUTH_VERSION = '1.0';
 
 app.use(express.json());
@@ -13,7 +13,7 @@ app.use(express.json());
 app.post('/generate-key', async (req, res) => {
     try {
         // Make API request to KeyAuth to generate a new key
-        const response = await axios.post('https://keyauth.cc/api/sell', {
+        const response = await axios.post('https://keyauth.com/api/', {
             name: KEYAUTH_APP_NAME,
             ownerid: KEYAUTH_OWNER_ID,
             version: KEYAUTH_VERSION,
